ab348ab147d7de41ced3429985a6be2dcc5b9844
Kevin Rojas <krojas1171@gmail.com>
2023-05-24 01:22:49 -0500

    Remove unused file

--- spec/shared_examples.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-RSpec.shared_examples 'generates an output shopping basket' do |_input, output|
-  it 'generates an output shopping basket' do
-    expect(subject).to eq(output)
-  end
-end
